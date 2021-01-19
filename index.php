bae868656ebb6e29be237a0a5e9a95bb26e49613
Lucas Delobelle <lucas@ld-web.net>
2021-01-19 14:49:45 +0100

    Classes, interfaces, héritage, polymorphisme, trait, composer

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\AbstractProduct;
+use App\Book;
+use App\Clothing;
+use App\IDisplayable;
+
+// Impossible d'instancier AbstractProduct car classe abstraite
+// $product = new AbstractProduct();
+// var_dump($product);
+
+/**
+ * Gets all products
+ *
+ * @return AbstractProduct[]
+ */
+function getProducts(): array
+{
+  return [
+    new Clothing(),
+    new Book(),
+    new Clothing()
+  ];
+}
+
+$dress = new Clothing();
+var_dump($dress);
+
+$book = new Book();
+var_dump($book);
+
+$products = [$dress, $book];
+
+foreach ($products as $product) {
+  if ($product instanceof IDisplayable) {
+    $product->display();
+  }
+  echo "<br />" . $product->getFullPrice() . "<br />";
+}
+
+$productsList = getProducts();
+
+foreach ($productsList as $product) {
+  echo "Prix : " . $product->getFullPrice() . " €<br />";
+}
